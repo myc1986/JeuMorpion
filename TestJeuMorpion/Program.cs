9f37b1d1416c3b8820d944991e272cf590fe56ae
myc1986 <moratinos.c@gmail.com>
2015-02-05 02:15:51 +0100

    Mise à jour des règles de victoire

---
+++ TestJeuMorpion/Program.cs
@@ -0,0 +1,19 @@
+﻿using APIJeuMorpionCsharp.Classes;
+using APIJeuMorpionCsharp.Globales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJeuMorpion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartieMorpion unePartie = new PartieMorpion();
+            unePartie.DemarrerPartie();
+        }
+    }
+}
